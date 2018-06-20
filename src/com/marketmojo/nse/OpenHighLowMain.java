44dfaacdcfde7c9f9c996099778eb46de18cbf9d
Sushil.Gupta <Sushil.Gupta@ATMECSINLT-092.ATMECS.CORP>
2018-06-20 07:53:47 +0530

    mojo commit1

---
+++ src/com/marketmojo/nse/OpenHighLowMain.java
@@ -0,0 +1,17 @@
+package com.marketmojo.nse;
+
+public class OpenHighLowMain {
+
+	public OpenHighLowMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OpenHighLow open = new OpenHighLow();
+		open.openHigh();
+	
+
+	}
+
+}
